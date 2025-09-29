45289039cb87a9842ea05a48b06692e911ee7159
ikbakk <ikbakfir@gmail.com>
2025-09-29 18:33:12 +0800

    adjust safe area

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TamaguiProvider, useTheme as useTamaguiTheme } from 'tamagui';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { TamaguiProvider } from 'tamagui';
 import appConfig from '../tamagui.config';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider, useTheme } from './contexts/ThemeProvider';
@@ -8,18 +8,15 @@ import AppNavigator from './navigation/AppNavigator';
 
 function AppContent() {
   const { theme } = useTheme();
-  const tamaguiTheme = useTamaguiTheme();
 
   // Set StatusBar style based on theme
   const barStyle = theme === 'dark_teal' ? 'light-content' : 'dark-content';
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, backgroundColor: tamaguiTheme.background.get() }}
-    >
+    <SafeAreaProvider>
       <StatusBar barStyle={barStyle} />
       <AppNavigator />
-    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
